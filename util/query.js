5310e384341bd87d11c2c05f8f5449989405bbc2
Will O'Beirne <wbobeirne@gmail.com>
2017-06-01 18:11:27 -0400

    Query for heroku

--- util/query.js
+++ util/query.js
@@ -1,13 +1,37 @@
 const pg = require("pg");
+const url = require("url");
 
 // Create the config object
-const config = {
-	database: process.env.DB_NAME,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST || "localhost",
-	port: process.env.DB_PORT || 5432,
-};
+let config;
+
+if (process.env.DATABASE_URL) {
+	const params = url.parse(process.env.DATABASE_URL);
+	const auth = params.auth.split(":");
+
+	config = {
+		database: params.pathname.split('/')[1],
+		user: auth[0],
+		password: auth[1],
+		host: params.hostname,
+		port: params.port,
+		ssl: true
+	};
+}
+else {
+	config = {
+		database: process.env.DB_NAME,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		host: process.env.DB_HOST || "localhost",
+		port: process.env.DB_PORT || 5432,
+	};
+}
+
+// Handle missing config args
+if (!config.user || !config.database) {
+	console.error("Missing database configuration!", config);
+	process.exit(1);
+}
 
 // Create the shared pool object
 const pool = new pg.Pool(config);
